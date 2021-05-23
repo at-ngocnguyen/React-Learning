a656eefafc382608676420ba3a7720cd569b562b
Ngoc Nguyen T. S <ngoc.nguyen@monstar-lab.com>
2021-05-23 17:30:25 +0700

    format router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import PathRouter from './router/PathRouter';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from 'share/home/home';
+import Clock from 'container/Clock';
+import Color from 'components/color';
+import Hobby from 'container/Hobby';
+import { Pagination } from 'react-bootstrap';
+import Todo from 'container/Todo';
+import Product from 'container/Product';
+import Error404 from 'share/404/404';
+import Header from 'share/header/header';
+import Footer from 'share/footer/footer';
 
 
 function App() {
   return (
-    <div className="container">
-      <PathRouter />
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/clock">
+              <Clock />
+            </Route>
+            <Route path="/color">
+              <Color />
+            </Route>
+            <Route path="/hobby">
+              <Hobby />
+            </Route>
+            <Route path="/pagination">
+              <Pagination />
+            </Route>
+            <Route path="/todo">
+              <Todo />
+            </Route>
+            <Route path="/product">
+              <Product />
+            </Route>
+            <Route path="">
+              <Error404 />
+            </Route>
+          </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
